3c99222b4226b7ce69db1fd712ab1dbdbc095836
martin <vazquezmartin1240@gmail.com>
2024-10-12 22:55:11 -0600

    primer commite

---
+++ CLINICA/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-2_%x8qnb7!@19kr9i+)_c(m)@*6l9+4#x5j^ef50&f$xgtc4^l"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
